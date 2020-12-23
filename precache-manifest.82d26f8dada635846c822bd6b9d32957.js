9445783733ddb5f9139f8cdb9386d4b9d9cefde0
EdwardBrodskiy <49645295+EdwardBrodskiy@users.noreply.github.com>
2020-12-23 11:40:41 +0000

    Updates

---
+++ precache-manifest.82d26f8dada635846c822bd6b9d32957.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4891ebfebf577c5845bf8dd3bc591d75",
+    "url": "/gh-test/index.html"
+  },
+  {
+    "revision": "a90cefe98577bb53809e",
+    "url": "/gh-test/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "7975a05d33d9290d3187",
+    "url": "/gh-test/static/js/2.4647f075.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/gh-test/static/js/2.4647f075.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a90cefe98577bb53809e",
+    "url": "/gh-test/static/js/main.2cbc7fe4.chunk.js"
+  },
+  {
+    "revision": "75db477f574b01422730",
+    "url": "/gh-test/static/js/runtime-main.d39e4229.js"
+  }
+]);
