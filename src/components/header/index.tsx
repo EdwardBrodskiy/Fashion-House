6af428f31dcc7cad04de5efc81b7309267cf0d8e
EdwardBrodskiy <49645295+EdwardBrodskiy@users.noreply.github.com>
2021-05-26 21:52:56 +0100

    disabled the large header feature

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Box, Flex, useColorMode, Image, Center } from '@chakra-ui/react'
 import { DarkModeToggle } from '../DarkMode'
 import logo_pic from '../../logo_pic_wide.svg'
@@ -15,7 +15,7 @@ const style = {
     }
   },
   short: {
-    mb: '12vh',
+    mb: '0',
     height: '12vh',
     shadow: '',
     logo_name: {
@@ -29,27 +29,17 @@ export const Header = () => {
   const { colorMode } = useColorMode()
   const bgColor = { light: '#00152d', dark: '#00152d' }
 
-  const [heightMode, setHeightMode] = useState<keyof typeof style>('long')
-
-  var last_pos = 0
+  const [heightMode, setHeightMode] = useState<keyof typeof style>('short')
 
   window.onscroll = () => {
-    const velocity = document.documentElement.scrollTop - last_pos
-    last_pos = (document.documentElement.scrollTop * 2 + last_pos) / 4
-    if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 0) {
-      if (heightMode != 'short') {
-        // document.documentElement.scrollTo({ top: 1 })
-        setHeightMode('short')
 
-      }
-
-    } else if (velocity < -30) {
-      setHeightMode('long')
+    if (heightMode != 'short' && document.documentElement.scrollTop > 0) {
+      setHeightMode('short')
     }
   }
 
   return (
-    <Box h={style[heightMode].height} mb={style[heightMode].mb} transition='0.5s ease-in-out'>
+    <Box h={style[heightMode].height} mb={style[heightMode].mb} transition='.2s ease_in' onTransitionEndCapture={() => document.documentElement.scrollTo({ top: 0, behavior: 'smooth' })}>
       <Box
         p={4}
         h={style[heightMode].height}
@@ -59,7 +49,8 @@ export const Header = () => {
         right='0'
         top='0'
         width='full'
-        transition='inherit'
+        transition='.5s ease-in-out'
+
         boxShadow={style[heightMode].shadow}
 
       >
