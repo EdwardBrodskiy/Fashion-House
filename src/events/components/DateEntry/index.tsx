eecf7ec9284e1889ac9c6cb68e7da144207f2bc0
EdwardBrodskiy <49645295+EdwardBrodskiy@users.noreply.github.com>
2021-05-31 18:35:31 +0100

    added events back to 2006

--- src/events/components/DateEntry/index.tsx
+++ src/events/components/DateEntry/index.tsx
@@ -21,7 +21,7 @@ export const DateEntry = ({ event }: Props) => {
               <AccordionIcon />
             </AccordionButton>
           </h2>
-          <AccordionPanel >
+          <AccordionPanel p='0'>
             <Heading my={4}>{event.title}</Heading>
             <Text mb={10} size='sm' >{event.date}</Text>
             {parse_image_and_text_list(event.content, `/imgs/events/${event.key}`, { px: '10%', mb: 4 }, { my: 16 })}
